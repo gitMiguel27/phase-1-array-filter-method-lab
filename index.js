7426bde19ead8ccbe8ec61af006d4782459b8a66
Miguel Nazario <mnazario35@gmail.com>
2022-02-02 17:03:06 -0500

    commit tree 67b4058ece9b887b36246a647260d0ede1a5f0f6

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(array, string) {
+    const drivers = array.filter(function(str) {
+        return str === string || str === string.toLowerCase() || str === string.toUpperCase();
+    })
+    return drivers;
+}
+
+function fuzzyMatch(array, string) {
+    const drivers = array.filter(function(str) {
+        return str.substr(0,2) === string;
+    })
+    return drivers;
+}
+
+function matchName(arrayofObj, string) {
+    const drivers = arrayofObj.filter(function(element) {
+        return element.name === string;
+    })
+    return drivers;
+}
